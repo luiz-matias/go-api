a6d20a8482db95fffee57666514525beaeaf316f
Luiz Matias <luiz.matias1999@gmail.com>
2025-03-20 21:39:28 -0300

    feat: added route handling to interact with usecase

--- controller/product_controller.go
+++ controller/product_controller.go
@@ -1,26 +1,108 @@
 package controller
 
 import (
+	"go-api/dto"
 	"go-api/model"
+	"go-api/usecase"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
 
 type ProductController struct {
-	//UseCase there
+	productUseCase usecase.ProductUseCase
 }
 
-func NewProductController() ProductController {
-	return ProductController{}
+func NewProductController(usecase usecase.ProductUseCase) ProductController {
+	return ProductController{
+		productUseCase: usecase,
+	}
 }
 
 func (p *ProductController) GetProducts(ctx *gin.Context) {
-	products := []model.Product{
-		{ID: 1, Name: "Laptop", Price: 5000, Quantity: 10},
-		{ID: 1, Name: "Mouse", Price: 2000, Quantity: 15},
-		{ID: 1, Name: "Headphone", Price: 3000, Quantity: 20},
+	products, err := p.productUseCase.GetProducts()
+
+	if err != nil {
+		response := dto.ErrorResponse{
+			Message:    "Internal server error",
+			Stacktrace: err.Error(),
+		}
+		ctx.JSON(http.StatusInternalServerError, response)
+		return
 	}
 
 	ctx.JSON(http.StatusOK, products)
+}
+
+func (p *ProductController) CreateProduct(ctx *gin.Context) {
+
+	var productModel model.Product
+	err := ctx.BindJSON(&productModel)
+
+	if err != nil {
+		response := dto.ErrorResponse{
+			Message:    "Internal server error",
+			Stacktrace: err.Error(),
+		}
+		ctx.JSON(http.StatusInternalServerError, response)
+		return
+	}
+
+	product, err := p.productUseCase.CreateProduct(productModel)
+
+	if err != nil {
+		response := dto.ErrorResponse{
+			Message:    "Internal server error",
+			Stacktrace: err.Error(),
+		}
+		ctx.JSON(http.StatusInternalServerError, response)
+		return
+	}
+
+	ctx.JSON(http.StatusCreated, gin.H{
+		"message": "Product created successfully",
+		"product": product,
+	})
+}
+
+func (p *ProductController) GetProduct(ctx *gin.Context) {
+	id := ctx.Param("id")
+
+	if id == "" {
+		response := dto.MessageResponse{
+			Message: "Invalid product id",
+		}
+		ctx.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	productId, err := strconv.Atoi(id)
+
+	if err != nil {
+		response := dto.MessageResponse{
+			Message: "Invalid product id",
+		}
+		ctx.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	product, err := p.productUseCase.GetProductByID(productId)
+
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"message":    "Internal server error",
+			"stacktrace": err.Error(),
+		})
+		return
+	}
+
+	if product == nil {
+		ctx.JSON(http.StatusNotFound, gin.H{
+			"message": "Product not found",
+		})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, product)
 }
