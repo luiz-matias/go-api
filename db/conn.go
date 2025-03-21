6a580cb7baaf8ef01b1314652a65e7f5c6a38ae6
Luiz Matias <luiz.matias1999@gmail.com>
2025-03-20 21:38:24 -0300

    feat: added postgres connection config

---
+++ db/conn.go
@@ -0,0 +1,35 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "1234"
+	dbname   = "postgres"
+)
+
+func GetConnection() *sql.DB {
+	psqlInformation := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		host,
+		port,
+		user,
+		password,
+		dbname,
+	)
+
+	db, err := sql.Open("postgres", psqlInformation)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Successfully connected to database " + dbname + "!")
+	return db
+}
